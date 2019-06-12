094ff2e0e09aca1670b0dab686b99c2d25e138b1
Ellie <3490717780@qq.com>
2019-06-12 14:15:38 +0800

    实现添加、删除、批量删除、修改功能

--- src/router.js
+++ src/router.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { Router, Route, Switch } from 'dva/router';
 import IndexPage from './routes/IndexPage';
-import CustomerPage from './routes/CustomerPage';
+import OrderLinePage from './routes/OrderLinePage'
+import CustomerPage from './routes/CustomerPage'
+import OrderPage from './routes/OrderPage'
+
 function RouterConfig({ history }) {
   return (
-    <Router history={history}>
-      <Switch>
-        <Route path="/" exact component={IndexPage} />
-        <Route path="/customer" exact component={CustomerPage} />
-      </Switch>
-    </Router>
+      <Router history={history}>
+        <Switch>
+          <Route path="/" exact component={IndexPage} />
+          <Route path="/orderLine" exact component={OrderLinePage} />
+          <Route path="/customer" exact component={CustomerPage} />
+          <Route path="/order" exact component={OrderPage} />
+        </Switch>
+      </Router>
+    
   );
 }
 
